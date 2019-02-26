d4141aac61982e6d31b4a8d9a3523214d7ccd878
faristoriqu <44657860+faristoriqu@users.noreply.github.com>
2019-02-26 20:08:41 +0700

    Update safina.php

--- application/views/safina.php
+++ application/views/safina.php
@@ -1,17 +1,73 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title>sapik</title>
-</head>
+<title>Kunyatakan Cinta <3 </title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="http://www.javascriptbank.com/" />
+<meta name="distribution" content="Global" />
+<meta name="copyright" content="JavaScriptBank.com" />
+<script type="text/javascript">
+fl=1
+function f1()
+{
+alert("Makasih yaaa... tolong kirim sms ke aku yaa... ")}
+function f(){
+if(fl==1)
+{
+Bn.style.top=90
+Bn.style.left=500
+fl=2
+}
+else if(fl==2)
+{
+Bn.style.top=90
+Bn.style.left=50
+fl=3
+}
+else if(fl==3)
+{
+Bn.style.top=235
+Bn.style.left=360
+fl=1
+}
+}
+</script>
+<style type="text/css">
+<!--
+body {
+background-color: #00FFFF;
+}
+.style1 {
+color: #000000;
+font-size: 24px;
+}
+.style2 {color: #000000; font-size: 18px; }
+.style3 {
+color: #FF00FF;
+font-weight: bold;
+font-size: 24px;
+}
+-->
+</style></head>
 <body>
-	<center>
-		<div>	
-			<h1>Selamat Datang Di kelompok kami.....</h1>
-		</div>
-		<img src="<?php echo base_url().'assets/'?>index.jpg" alt="">
-		<div>	
-			tertanda PO, Syavina OK...
-		</div>
-	</center>
-</body>
+
+<div id="By" style="position:absolute; left:615px; top:380px; width:50px;height:198px;">
+
+<input type="button" value=" YES " onClick="f1()" />
+</div>
+<div ID="Bn" style="position:absolute; left:688px; top:380px; width:210px; height:210px;">
+<input name="button" type="button" onMouseOver="f()" value=" NO " />
+</div>
+
+<p align="center" class="style1">WAJAHMU BAGAI SINAR REMBULAN YANG MENYINARI HATIKU *_*</p>
+<hr> <hr>
+<p align="center" class="style2">Sejak Pertama Mengenalmu,batapa senang hatiku..!!</p>
+<p align="center" class="style2">Tapi rasaku ini t' pernah sampai kehatimu dengan nyali dan lisan yang kupunya</p>
+<p align="center" class="style2">Dan sebenarnya rasa ini t' pernah bohong untuk memungkiri semua ini</p>
+<p align="center" class="style2">Dari itu kutempuh kuliah design web untuk menyuntikan script ini kelubuk hatimu </p>
+<p align="center" class="style2">Sekarang waktunya kusampaikan itu...</p>
+<div align="center" class="style3">MAUKAH KAU JADI KEKASIH HATIKU ?????
+</div>
+<p class="style1">  </p>
+<p> </p>
+</BODY>
 </html>

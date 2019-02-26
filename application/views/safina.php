e460bf4f41a294470f408c3279e50b0a4e91e0ed
faristoriqu <44657860+faristoriqu@users.noreply.github.com>
2019-02-26 20:01:04 +0700

    awalan

--- application/views/safina.php
+++ application/views/safina.php
@@ -5,7 +5,13 @@
 </head>
 <body>
 	<center>
+		<div>	
+			<h1>Selamat Datang Di kelompok kami.....</h1>
+		</div>
 		<img src="<?php echo base_url().'assets/'?>index.jpg" alt="">
+		<div>	
+			tertanda PO, Syavina OK...
+		</div>
 	</center>
 </body>
 </html>
